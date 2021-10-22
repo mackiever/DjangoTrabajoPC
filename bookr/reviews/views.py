5594c8869d5f5feaf52d7eca1d04d4e6ac5a5295
maxmac <288struggle@gmail.com>
2021-10-22 12:22:38 -0400

    new updates

--- bookr/reviews/views.py
+++ bookr/reviews/views.py
@@ -4,3 +4,7 @@ def index(request):
     #viewvar = "Max Macavilca"
     return render(request, "base.html")
     #page 67
+
+def search_view(request):
+    user_search = request.GET.get("user_search") or "Please enter a book title in order to search."
+    return render(request, "search_result.html", {"search_result" : user_search})
